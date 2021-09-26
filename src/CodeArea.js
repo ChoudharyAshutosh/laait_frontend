abeb918529eb30b9bfcbcaf0857d226436a3c5fd
Ashutosh Choudhary <ashu@pop-os.localdomain>
2021-09-26 17:10:36 +0530

    added code area

--- src/CodeArea.js
+++ src/CodeArea.js
@@ -1,9 +1,93 @@
 import React  from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
 import './CodeArea.css';
 export default function CodeArea(){
     return(
         <div className="codearea">
-
+            <div className="code-container">
+                <div className="code-line">
+                    <div className="code-input">
+                        <div className="play-icon"><FontAwesomeIcon icon={faPlay} /></div>
+                        <div className="code-line-container">
+                            <textarea className="code" rows={1} autocorrect="off" autocapitalize="none" spellcheck="false" tabindex="0" wrap="off"></textarea>
+                        </div>
+                    </div>
+                    <div className="code-output">DFVZDFVDF</div>
+                </div>
+                <div className="code-line">
+                    <div className="code-input">
+                        <div className="play-icon"><FontAwesomeIcon icon={faPlay} /></div>
+                        <div className="code-line-container">
+                            <textarea className="code" rows={1} autocorrect="off" autocapitalize="none" spellcheck="false" tabindex="0" wrap="off"></textarea>
+                        </div>
+                    </div>
+                    <div className="code-output"></div>
+                </div>
+                <div className="code-line">
+                    <div className="code-input">
+                        <div className="play-icon"><FontAwesomeIcon icon={faPlay} /></div>
+                        <div className="code-line-container">
+                            <textarea className="code" rows={1} autocorrect="off" autocapitalize="none" spellcheck="false" tabindex="0" wrap="off"></textarea>
+                        </div>
+                    </div>
+                    <div className="code-output"></div>
+                </div>
+                <div className="code-line">
+                    <div className="code-input">
+                        <div className="play-icon"><FontAwesomeIcon icon={faPlay} /></div>
+                        <div className="code-line-container">
+                            <textarea className="code" rows={1} autocorrect="off" autocapitalize="none" spellcheck="false" tabindex="0" wrap="off"></textarea>
+                        </div>
+                    </div>
+                    <div className="code-output"></div>
+                </div>
+                <div className="code-line">
+                    <div className="code-input">
+                        <div className="play-icon"><FontAwesomeIcon icon={faPlay} /></div>
+                        <div className="code-line-container">
+                            <textarea className="code" rows={1} autocorrect="off" autocapitalize="none" spellcheck="false" tabindex="0" wrap="off"></textarea>
+                        </div>
+                    </div>
+                    <div className="code-output"></div>
+                </div>
+                <div className="code-line">
+                    <div className="code-input">
+                        <div className="play-icon"><FontAwesomeIcon icon={faPlay} /></div>
+                        <div className="code-line-container">
+                            <textarea className="code" rows={1} autocorrect="off" autocapitalize="none" spellcheck="false" tabindex="0" wrap="off"></textarea>
+                        </div>
+                    </div>
+                    <div className="code-output"></div>
+                </div>
+                <div className="code-line">
+                    <div className="code-input">
+                        <div className="play-icon"><FontAwesomeIcon icon={faPlay} /></div>
+                        <div className="code-line-container">
+                            <textarea className="code" rows={1} autocorrect="off" autocapitalize="none" spellcheck="false" tabindex="0" wrap="off"></textarea>
+                        </div>
+                    </div>
+                    <div className="code-output"></div>
+                </div>
+                <div className="code-line">
+                    <div className="code-input">
+                        <div className="play-icon"><FontAwesomeIcon icon={faPlay} /></div>
+                        <div className="code-line-container">
+                            <textarea className="code" rows={1} autocorrect="off" autocapitalize="none" spellcheck="false" tabindex="0" wrap="off"></textarea>
+                        </div>
+                    </div>
+                    <div className="code-output"></div>
+                </div>
+                <div className="code-line">
+                    <div className="code-input">
+                        <div className="play-icon"><FontAwesomeIcon icon={faPlay} /></div>
+                        <div className="code-line-container">
+                            <textarea className="code" rows={1} autocorrect="off" autocapitalize="none" spellcheck="false" tabindex="0" wrap="off"></textarea>
+                        </div>
+                    </div>
+                    <div className="code-output"></div>
+                </div>
+            </div>
         </div>
     )
 }
